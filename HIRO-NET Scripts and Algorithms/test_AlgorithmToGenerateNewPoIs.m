6b12804060e90466b3a86996ae7deb1953ceb1b9
HIRO-NET <33672845+HIRO-NET-Emergency-Network@users.noreply.github.com>
2017-11-16 15:41:19 -0500

    Update test_AlgorithmToGenerateNewPoIs.m

--- HIRO-NET Scripts and Algorithms/test_AlgorithmToGenerateNewPoIs.m
+++ HIRO-NET Scripts and Algorithms/test_AlgorithmToGenerateNewPoIs.m
@@ -154,4 +154,24 @@ distance_min(mod(distance_min,2)<1) = distance_min(mod(distance_min,2)<1)-1; % W
 [x_NEW_POIS,y_NEW_POIS,old_poi_narrow,new_poi_narrow] = generateNewPoIsPathAware(MAX_NEW_POIS,N_samples,variance_gaussian_narrow,X_axis,Y_axis,n,x_original_pois,y_original_pois,x_added_but_old_pois,y_added_but_old_pois,old_poi_broad_temporary,distance_min,y);
 
 
-
+% my_frame
+% LPD/LPI Scheme Transmitter
+% Copyright (c) 2016 Emrecan Demirors and Tommaso Melodia, WiNES Lab, Northeastern University,
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
