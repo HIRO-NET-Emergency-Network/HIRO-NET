126943f1e78d92dc08229a0ce457698271d8c805
HIRO-NET <33672845+HIRO-NET-Emergency-Network@users.noreply.github.com>
2017-11-16 15:40:14 -0500

    Update computeOptimalSearchFlightPlan.m

--- HIRO-NET Scripts and Algorithms/computeOptimalSearchFlightPlan.m
+++ HIRO-NET Scripts and Algorithms/computeOptimalSearchFlightPlan.m
@@ -1,3 +1,5 @@
+
+
 function [FT_BASE,FT_POI,longestPath,G,GLP] = computeOptimalSearchFlightPlan(x_BASES,y_BASES,x_fordrone,y_fordrone,speed,N_poi,MAX_FLIGHT_TIME,REWARDS,index_base)
 %COMPUTEOPTIMALSEARCHFLIGHTPLAN This function computes the optimal flight plan for a given drone.
 %   This function computes the optimal flight plan that maximizes the
@@ -38,3 +40,25 @@ G = digraph(matrix_adj);
 GLP = digraph(xxx);
 
 end
+
+% my_frame
+% LPD/LPI Scheme Transmitter
+% Copyright (c) 2016 Emrecan Demirors and Tommaso Melodia, WiNES Lab, Northeastern University,
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
