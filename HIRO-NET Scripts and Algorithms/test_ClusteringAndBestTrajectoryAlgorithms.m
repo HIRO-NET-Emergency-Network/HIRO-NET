908f4f7ae59e3810ba767e71038972647a625ce8
HIRO-NET <33672845+HIRO-NET-Emergency-Network@users.noreply.github.com>
2017-12-03 21:55:18 -0500

    Update test_ClusteringAndBestTrajectoryAlgorithms.m

--- HIRO-NET Scripts and Algorithms/test_ClusteringAndBestTrajectoryAlgorithms.m
+++ HIRO-NET Scripts and Algorithms/test_ClusteringAndBestTrajectoryAlgorithms.m
@@ -300,7 +300,7 @@ legend('PoIs, Drone 1 - Base 1', ...
 
 % my_frame
 % LPD/LPI Scheme Transmitter
-% Copyright (c) 2016 Emrecan Demirors and Tommaso Melodia, WiNES Lab, Northeastern University,
+% Copyright (c) 2017 WiNES Lab, Northeastern University,
 %
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
